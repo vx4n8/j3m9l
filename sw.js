d1fc4d1ca2eb6534bc2bef451d39a04f38f93546
Patrice Roblin App <roblinpatrice@gmail.com>
2026-03-27 12:13:49 +1000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE = 'rekon-callsheet-v1';
+const ASSETS = [
+  '/j3m9l/',
+  '/j3m9l/callsheet-5.html',
+  '/j3m9l/manifest.json',
+  '/j3m9l/sw.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      if (cached) return cached;
+      return fetch(e.request).then(response => {
+        if (response && response.status === 200) {
+          const clone = response.clone();
+          caches.open(CACHE).then(cache => cache.put(e.request, clone));
+        }
+        return response;
+      }).catch(() => caches.match('/j3m9l/callsheet-5.html'));
+    })
+  );
+});
